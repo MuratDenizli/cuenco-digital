f758ee360304887e37afc3f0d35fd89c86cf9d5b
Murat Denizli <muratdenizli0@gmail.com>
2022-09-18 20:12:43 +0300

    butonların aktiflif ayarları tamamlandı

--- src/components/box/box.js
+++ src/components/box/box.js
@@ -3,17 +3,18 @@ import './index.css';
 import logo from './../../assets/images/yemek.jpeg';
 
 function Box(props) {
+    const { name, description, price, img } = props.data;
     return (
         <div className='box'>
             <div className='img'>
                 <img src={logo}></img>
             </div>
             <div className='content'>
-                <span className='name'>Fried Chicken</span>
-                <span className='description'>Golden brown fried Chicken</span>
+                <span className='name'>{name}</span>
+                <span className='description'>{description}</span>
                 <div className='price-area'>
                     <span className='price-icon'>&#x20BA;</span>
-                    <span className='price'> 100</span>
+                    <span className='price'> {price}</span>
                 </div>
             </div>
         </div>
--- src/pages/homepage/index.js
+++ src/pages/homepage/index.js
@@ -3,8 +3,22 @@ import cuenco from './../../assets/images/cuenco-text.svg';
 import chevronLeft from './../../assets/icons/chevron-left-solid.svg';
 import Button from '../../components/button/button';
 import Box from '../../components/box/box';
+import cuencoData from '../../../src/data/cuenco-digital.json'
+import { useState } from 'react';
 
 function Homepage() {
+    const [menuData, setMenuData] = useState(cuencoData.data);
+    const [active, setActive] = useState(cuencoData.data[0]);
+    const [filterList, setFilterList] = useState(cuencoData.data[0].list);
+
+    const handleAnchorClick = (event, name) => {
+        event.preventDefault();
+        const active = cuencoData.data.find(item => item.name === name);
+        setActive(active);
+        const filterList = cuencoData.data.find(item => item.name === name).list;
+        setFilterList(filterList);
+    };
+
     return (
         <div>
             <div className="header">
@@ -13,19 +27,17 @@ function Homepage() {
                 </div> */}
                 <img className='cuenco-text' src={cuenco} alt='cuenco-text' />
             </div>
-            <div className='categories'>
-                <Button name="Burgerler" isActive={true}/>
-                <Button name="Soğuk İçecekler"/>
-                <Button name="Bowl"/>
-                <Button name="Sıcak İçecekler"/>
-            </div>
-            <div>
-                <Box/>
-                <Box/>
-                <Box/>
-                <Box/>
-                <Box/>
-                <Box/>
+            <div className='menu'>
+                <div className='categories'>
+                    {menuData.map((data, i) =>
+                        <Button key={i} isActive={active.name == data.name} name={data.name} onClick={(e) => handleAnchorClick(e, data.name)} />
+                    )}
+                </div>
+                <div>
+                    {filterList.map((data, i) =>
+                        <Box key={i} data={data} />
+                    )}
+                </div>
             </div>
         </div>
     );
